c0dbfee06990058590dedb9b55e1d75c357e75a3
jundong.li <lijundong@100tal.com>
2018-08-30 16:50:00 +0800

    去模糊图片完成

--- vague_image.py
+++ vague_image.py
@@ -2,37 +2,34 @@
 import cv2
 import os
 import sys
-# for root, dir, files in os.walk('E:\\picture_process\\images'):
-#     for image in files:
-#         print(image)
-#         image = cv2.imread('E:\\picture_process\\images\\' + image)
-#         img2gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         # imgeVar的值决定了图片的模糊度。
-#         # imgeVar数值越大，图片越清晰
-#         imageVar = cv2.Laplacian(img2gray, cv2.CV_64F).var()
-#         print(imageVar)
+import subprocess
 
-def test(images_path):
+def test(images_path,threshold):
     pure_path = []
+    dirty_path = []
     count = 0
     for root, dirs, files in os.walk(images_path):
         if count == 0:
             for dir in dirs:
-                pure_path.append(root+'/'+dir+'/pure')
+                pure_path.append(root+dir+'/pure')
+                dirty_path.append(root+dir+'/dirty')
         count += 1
+
     print(pure_path)
-    for singgle_pure_path in pure_path:
-        for root, dir, files in os.walk(pure_path):
+    for single_pure_path, single_dirty_path in zip(pure_path,dirty_path):
+        for root, dir, files in os.walk(single_pure_path):
             for image in files:
                 print(image)
-                image = cv2.imread(pure_path + '/' + image)
-                img2gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                img = cv2.imread(single_pure_path + '/' + image)
+                img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 # imgeVar的值决定了图片的模糊度。
                 # imgeVar数值越大，图片越清晰
                 imageVar = cv2.Laplacian(img2gray, cv2.CV_64F).var()
                 print(imageVar)
-
-
+                if imageVar < int(threshold):
+                    result = subprocess.call('mv '+single_pure_path+'/'+image + ' ' + single_dirty_path, shell=True)
+                    print(result)
 if __name__ == '__main__':
     images_path = sys.argv[1]
-    test(images_path = images_path)
+    threshold = sys.argv[2]
+    test(images_path, threshold)
